a485feab9412e37fce581c5b515c011e5a0e6cf8
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-01-10 22:47:32 +0600

    name spacing added

---
+++ laracast_php/section_4/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3307,
+        'dbname' => 'mynotes',
+        'charset' => 'utf8mb4'
+    ],
+
+    //
+];

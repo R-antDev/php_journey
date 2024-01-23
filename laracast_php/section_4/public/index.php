93be08fe817be2beb5d6f035cc0aee01bfbd8a77
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-01-23 12:09:46 +0600

    laracast section 6 episode 45

--- laracast_php/section_4/public/index.php
+++ laracast_php/section_4/public/index.php
@@ -8,6 +8,8 @@ error_reporting(E_ALL);
 session_start();
 
 use Core\Router;
+use Core\Session;
+
 const BASE_PATH = __DIR__.'/../';
 
 require BASE_PATH.'Core/functions.php';
@@ -28,3 +30,5 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
+
+Session::unflash();
--- laracast_php/section_4/views/partials/head.php
+++ laracast_php/section_4/views/partials/head.php
@@ -2,7 +2,7 @@
 <html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
-    <title>Lara-cast</title>
+    <title>PHP Beginner</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 </head>
 

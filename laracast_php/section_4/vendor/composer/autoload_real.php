a4a31cf8d7e336ee710887d243bf74175c5814a3
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-01-26 18:40:49 +0600

    laracast episode 48 complete

--- laracast_php/section_4/vendor/composer/autoload_real.php
+++ laracast_php/section_4/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitbbfb33d32ef030d85addee14503459aa
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitbbfb33d32ef030d85addee14503459aa', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitbbfb33d32ef030d85addee14503459aa', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitbbfb33d32ef030d85addee14503459aa
         call_user_func(\Composer\Autoload\ComposerStaticInitbbfb33d32ef030d85addee14503459aa::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitbbfb33d32ef030d85addee14503459aa::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
